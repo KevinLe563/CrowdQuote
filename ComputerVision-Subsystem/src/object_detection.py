6d839b7ca9b56dfbc3da77a8420f3bf730a712db
KevinLe563 <55369024+KevinLe563@users.noreply.github.com>
2023-07-19 23:20:49 -0400

    Update tracker to be more accurate
    * add diff tracker
    
    * fix counter when person starts below or above line

--- ComputerVision-Subsystem/src/object_detection.py
+++ ComputerVision-Subsystem/src/object_detection.py
@@ -1,20 +1,22 @@
 from ultralytics import YOLO
 from pathlib import Path
-from object_tracking import Tracker
+from object_tracking import CentroidTracker
 from person import PersonObject
+import numpy as np
 import cv2
 import math
 import configparser
 
 
 # credit: https://github.com/MuhammadMoinFaisal/Computervisionprojects/blob/main/YOLOv8-CrashCourse/Running_YOLOv8_Video/YOLOv8_Video.py
+# credit: https://github.com/saimj7/People-Counting-in-Real-Time/tree/master
 
 class ObjectDetector():
     def __init__(self, config_path):
         self.config = configparser.ConfigParser()
         self.config.read(config_path)
 
-        self.tracker = Tracker()
+        self.tracker = CentroidTracker()
         self.model = str(Path(self.config['YOLO']['yolov8model']))
         self.output_video_path = str(Path(config_path, self.config['VIDEOS']['output_videos']).resolve())
         self.classes = [
@@ -31,12 +33,9 @@ class ObjectDetector():
             ] # all avaialble classes of the model, only person is used
         
     def detectObject(self, video_path, entrance_height=None, centroid_radius=5):
-        # tracks set of ids that crossed each line
-        crossed_top_line = set()
-        crossed_bottom_line = set()
-
-        people_exited = set()
-        people_entered = set()
+        tracked_persons = {}
+        total_entering = 0
+        total_exiting = 0
 
         video_capture=cv2.VideoCapture(video_path)
 
@@ -47,9 +46,6 @@ class ObjectDetector():
         model=YOLO(self.model)
 
         line_entrance_height = entrance_height if entrance_height else frame_height//2
-        height_offset = math.ceil(frame_height*0.05)
-        top_line_height = line_entrance_height - height_offset
-        bottom_line_height = line_entrance_height + height_offset
 
         try:
             success, img = video_capture.read()
@@ -58,14 +54,13 @@ class ObjectDetector():
                 # do frame by frame for video
                 results=model(img,stream=True)
                 # draw a line in the center of the image
-                self.drawEntranceExitLine(img, (0, top_line_height), (frame_width, top_line_height))
-                self.drawEntranceExitLine(img, (0, bottom_line_height), (frame_width, bottom_line_height))
-                cv2.putText(img, f'People Entering: {len(people_entered)}', (0, math.ceil(0.2*frame_height)), 0, 1, (0, 255, 0), thickness=1,lineType=cv2.LINE_AA)
-                cv2.putText(img, f'People Exiting: {len(people_exited)}', (0, math.ceil(0.3*frame_height)), 0, 1, (0, 255, 0), thickness=1,lineType=cv2.LINE_AA)
+                self.drawEntranceExitLine(img, (0, line_entrance_height), (frame_width, line_entrance_height))
+                cv2.putText(img, f'People Entering: {total_entering}', (0, math.ceil(0.2*frame_height)), 0, 1, (0, 255, 0), thickness=1,lineType=cv2.LINE_AA)
+                cv2.putText(img, f'People Exiting: {total_exiting}', (0, math.ceil(0.3*frame_height)), 0, 1, (0, 255, 0), thickness=1,lineType=cv2.LINE_AA)
 
                 # check each bounding box -> draw a rectangle and label it
                 for r in results:
-                    person_objects = []
+                    person_rects = []
                     boxes=r.boxes
                     for box in boxes:
                         object_class=int(box.cls[0])
@@ -73,34 +68,52 @@ class ObjectDetector():
                         if class_name == self.classes[0]: # check if it's a person, ignore other objects
                             x1, y1, x2, y2 = box.xyxy[0]
                             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-                            person_objects.append((x1, y1, x2, y2))
+                            person_rects.append((x1, y1, x2, y2))
                     
-                    person_objects_with_ids = self.utiliseTracker(person_objects)
-                    for person_object_with_id in person_objects_with_ids:
-                        x1,y1,x2,y2,id = person_object_with_id
+                    person_objects = self.utiliseTracker(person_rects)
+                    for (objectID, centroid) in person_objects.items():
+                        person_object = tracked_persons.get(objectID, None)
 
-                        bounding_box_width = x2-x1
-                        bounding_box_height = y2-y1
-                        center_x = x1 + bounding_box_width//2
-                        center_y = y1 + bounding_box_height//2
+                        if not person_object:
+                            person_object = PersonObject(objectID, centroid)
+                        else:
+                            # the difference between the y-coordinate of the *current*
+                            # centroid and the mean of *previous* centroids will tell
+                            # us in which direction the object is moving (negative for
+                            # 'up' and positive for 'down')
 
-                        self.labelObject(img, self.classes[0], person_object_with_id)
-                        # person crosses the top line -> assume top-bottom is exit and bottom-top is enter
-                        # tracks people exiting
-                        if center_y+centroid_radius > top_line_height and center_y-centroid_radius < top_line_height:
-                            crossed_top_line.add(id)
-                        if id in crossed_top_line and center_y+centroid_radius > bottom_line_height and center_y-centroid_radius < bottom_line_height:
-                            people_exited.add(id)
+                            # c[1] is the y value of centroid
+                            y = [c[1] for c in person_object.centroids]
+                            direction = centroid[1] - np.mean(y)
+                            person_object.centroids.append(centroid)
 
-                        # tracks people entering
-                        if center_y+centroid_radius > bottom_line_height and center_y-centroid_radius < bottom_line_height:
-                            crossed_bottom_line.add(id)
-                        if id in crossed_bottom_line and center_y+centroid_radius > top_line_height and center_y-centroid_radius < top_line_height:
-                            people_entered.add(id)
+                            # check to see if the object has been counted or not
+                            if not person_object.is_counted:
+                                # if the direction is negative (indicating the object
+                                # is moving up and entering) AND the centroid is above the center
+                                # line, count the object
+                                # also make sure object started below the line to start
+                                if direction < 0 and centroid[1] < line_entrance_height and person_object.centroids[0][1] > line_entrance_height:
+                                    total_entering += 1
+                                    person_object.is_counted = True
 
+                                # if the direction is positive (indicating the object
+                                # is moving down and is exiting) AND the centroid is below the
+                                # center line, count the object
+                                # also make sure object started above the line to start
+                                elif direction > 0 and centroid[1] > line_entrance_height and person_object.centroids[0][1] < line_entrance_height:
+                                    total_exiting += 1
+                                    person_object.is_counted = True
+                            
+                        # store the trackable object in our dictionary
+                        tracked_persons[objectID] = person_object
+                        print(person_object.centroids[0])
+                        self.labelObject(img, self.classes[0], person_object)
+                                    
         
                 output_video.write(img)
                 cv2.imshow("Image", img)
+                print('===')
                 if cv2.waitKey(1) & 0xFF==ord('q'):
                     break
         except Exception as e:
@@ -109,29 +122,27 @@ class ObjectDetector():
         video_capture.release()
         output_video.release()
         cv2.destroyAllWindows()
-        print(people_entered)
-        print(people_exited)
 
     def drawEntranceExitLine(self, img, coord1, coord2):
         cv2.line(img, coord1, coord2, (0, 255, 0), thickness=2)
 
     def labelObject(self, img, class_name, person_object, color=(255,0,255)):
-        x1,y1,x2,y2,id = person_object
-        cv2.rectangle(img, (x1,y1), (x2,y2), color, 1)
+        # x1,y1,x2,y2,id = person_object
+        # cv2.rectangle(img, (x1,y1), (x2,y2), color, 1)
         
-        label=f'{class_name} {id}'
-        t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=1)[0]
-        c2 = x1 + t_size[0], y1 - t_size[1] - 3
+        label=f'{class_name} {person_object.objectID}'
+        # t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=1)[0]
+        # c2 = x1 + t_size[0], y1 - t_size[1] - 3
         # bounding box
-        cv2.rectangle(img, (x1,y1), c2, color, -1, cv2.LINE_AA)  # filled
+        # cv2.rectangle(img, (x1,y1), c2, color, -1, cv2.LINE_AA)  # filled
         # centroid
-        bounding_box_width = x2-x1
-        bounding_box_height = y2-y1
-        center_x = x1 + bounding_box_width//2
-        center_y = y1 + bounding_box_height//2
-        cv2.circle(img, (center_x, center_y), 5, color, cv2.FILLED)
+        # bounding_box_width = x2-x1
+        # bounding_box_height = y2-y1
+        # center_x = x1 + bounding_box_width//2
+        # center_y = y1 + bounding_box_height//2
+        cv2.circle(img, person_object.centroids[-1], 5, color, cv2.FILLED)
         # class label
-        cv2.putText(img, label, (x1,y1-2),0, 1, (255, 255, 255), thickness=1,lineType=cv2.LINE_AA)
+        cv2.putText(img, label, (person_object.centroids[-1][0], person_object.centroids[-1][1]-2), 0, 1, (255, 255, 255), thickness=1,lineType=cv2.LINE_AA)
     
 
     def utiliseTracker(self, person_objects):
--- ComputerVision-Subsystem/src/person.py
+++ ComputerVision-Subsystem/src/person.py
@@ -1,8 +1,6 @@
 class PersonObject():
-    def __init__(self, coords, id):
+    def __init__(self, id, centroid):
         # coords = (x1, y1, x2, y2)
-        self.bounding_box = coords
-        self.id = id
-
-    def set_bounding_box(self, coords):
-        self.bounding_box = coords
+        self.objectID = id
+        self.centroids = [centroid]
+        self.is_counted = False
