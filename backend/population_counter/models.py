093bbc3c6f7fead257b6c1dec00fb6683734f083
k26le <k26le@uwaterloo.ca>
2023-07-20 23:39:25 -0400

    add models

--- backend/population_counter/models.py
+++ backend/population_counter/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from address.models import AddressField
 
 # Create your models here.
+# Django automatically adds id/primary key field
+
+# Location Model
+class Location(models.Model):
+    street_number = models.IntegerField
+    address = AddressField()
+    building_name = models.CharField(max_length=50)
+
+# Camera Model
+class Camera(models.Model):
+    # each camera must monitor 1 location
+    # if location is deleted, set the location_id to null. Camera can stay in database
+    location_id = models.ForeignKey(Location, null=True, on_delete=models.SET_NULL) 
+
+# Population Model
+class Population(models.Model):
+    # each population must belong to a location
+    # if location is deleted, population should be deleted as well
+    location_id = models.ForeignKey(Location, on_delete=models.CASCADE)
+    people_count = models.PositiveIntegerField()
+    # set timestamp to when the object is created
+    timestamp = models.DateTimeField(auto_now=True)
+
