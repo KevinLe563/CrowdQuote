d8ebfdd3c308bc07472eefd9dbdabc0a209bde2f
Carl Zhang <carlzproj@gmail.com>
2023-07-24 16:58:08 -0400

    fe: poll backend every 10s

--- frontend/src/components/Modal/index.js
+++ frontend/src/components/Modal/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import {
 	Chart as ChartJS,
 	LineElement,
@@ -26,39 +27,90 @@ ChartJS.register(
 	Legend
 );
 
-let populationData = {
-	data: [],
-};
-
-let purpose = "Dance Room";
-let address = "PAC, University Avenue West, Waterloo, ON, Canada";
+// let purpose = "Dance Room";
+// let address = "PAC, University Avenue West, Waterloo, ON, Canada";
 
 const Modal = ({ open, onClose, position }) => {
-	if (!open) return null;
+	const [address, setAddress] = useState();
+	const [purpose, setPurpose] = useState();
+	const [populationData, setPopulationData] = useState();
+
+	const fetchData = async () => {
+		await axios
+			.get("http://localhost:8000/api/location/", {
+				params: {
+					lat: 80.788,
+					lng: 45.34,
+					// lat: position.lat,
+					// lng: position.lng,
+				},
+			})
+			.then((res) => {
+				console.log(res);
+				const data = res.data;
+
+				axios
+					.get("http://localhost:8000/api/population/", {
+						params: {
+							location_id: data.id,
+						},
+					})
+					.then((res) => {
+						console.log(res);
+						setPopulationData(res.data);
+					})
+					.catch(() => console.log("Get Population Data Fail"));
+
+				setAddress(
+					`${data.civic_number} ${data.street_name} ${data.postal_code}`
+				);
+				setPurpose(`${data.building_name} ${data.room}`);
+			})
+			.catch(() => console.log("Get Location Fail"));
+	};
+
+	useEffect(() => {
+		if (!open) return () => {};
+
+		fetchData();
+		const interval = setInterval(fetchData, 10000);
+
+		return () => clearInterval(interval);
+	}, [open]);
+
+	if (!open) {
+		return null;
+	}
+
+	if (!address || !purpose || !populationData) return null;
+
+	// console.log(address);
+	// console.log(purpose);
+	// console.log(populationData);
 
 	// goal: request to backend api to get populationData given (lat/lng)
 	// current: mock static data for populationData
-	populationData.data = [];
-	let date = new Date();
-	for (let i = 1; i <= 144; i++) {
-		populationData.data.push({
-			location_id: i,
-			people_count: Math.floor(Math.random() * 100),
-			date_time: date,
-		});
-		date = new Date(date.setMinutes(date.getMinutes() + 10));
-	}
+	// populationData.data = [];
+	// let date = new Date();
+	// for (let i = 1; i <= 144; i++) {
+	// 	populationData.data.push({
+	// 		location_id: i,
+	// 		people_count: Math.floor(Math.random() * 100),
+	// 		date_time: date,
+	// 	});
+	// 	date = new Date(date.setMinutes(date.getMinutes() + 10));
+	// }
 
-	console.log(populationData);
+	console.log("pop data", populationData);
 	// console.log(populationData.data.map((obj) => obj.date_time));
 
 	const data = {
 		// labels: ["2022-11-01", "2022-11-02", "2022-11-03", "2022-11-04"],
-		labels: populationData.data.map((obj) => obj.date_time),
+		labels: populationData.map((obj) => obj.timestamp),
 		datasets: [
 			{
 				label: "people",
-				data: populationData.data.map((obj) => obj.people_count),
+				data: populationData.map((obj) => obj.people_count),
 				backgroundColor: "aqua",
 				borderColor: "black",
 				tension: 0.4,
